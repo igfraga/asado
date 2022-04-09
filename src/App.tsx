a7220e53ba6d2b8b0da3821f07533940182952e9
Ignacio Fraga <fraga.nacho@gmail.com>
2022-04-08 20:10:32 -0400

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,31 @@ import React from "react";
 import "./App.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Builder } from "./builder-form";
+import * as Rt from "react-router-dom";
+import * as Ch from "@chakra-ui/react";
+
+function Tafies() {
+
+  return (
+    <Ch.Link as={Rt.Link} to="/asado">Asado</Ch.Link>
+  )
+}
+
+function Routes() {
+  return (
+    <Rt.Routes>
+      <Rt.Route path="/" element={<Tafies/>}></Rt.Route>
+      <Rt.Route path="/asado" element={<Builder />}></Rt.Route>
+    </Rt.Routes>
+  );
+}
 
 function App() {
   return (
     <ChakraProvider>
-      <div className="App">
-        <Builder />
-      </div>
+      <Rt.BrowserRouter>
+        <Routes />
+      </Rt.BrowserRouter>
     </ChakraProvider>
   );
 }
